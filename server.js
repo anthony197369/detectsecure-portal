da2f156d6d200a42f1a95284389ae86acc459764
anthony197369 <157886894+anthony197369@users.noreply.github.com>
2026-02-19 12:18:32 +0000

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/api/test", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
